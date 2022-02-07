4fe9f7dabb50b1a392400466b8929c2ed5891cb7
Cyd Gabriel <46863120+cydgabriel@users.noreply.github.com>
2022-02-07 20:53:51 -0300

    Create Conceitos.py
    Conceitos Básicos da Linguagem Python

---
+++ Conceitos.py
@@ -0,0 +1,25 @@
+#Para escrever uma palavra se utiliza:
+print('Hello, World')
+
+#Para fazer uma equação matemática básica se escreve somente o cálculo
+1 + 1
+
+#Operadores matemáticos
++ = soma
+- = subtração
+/ = divisão
+* = multiplicação
+** = exponencial 
+^ = XOR (Operadores Bitwise)
+
+#Para se descobrir o tipo de uma variável se escreve 'Type(variavel)' exemplo:
+type(2)
+type(42.0)
+type('Hello, World!')
+
+#As classes se definem basicamente por Int = Inteiro, Float = Numero de ponto flutuante e Str = Letras em Sequencia em Cadeia
+
+#Para atribuir uma variavél se usa o '=', exemplo:
+x = 5
+
+
